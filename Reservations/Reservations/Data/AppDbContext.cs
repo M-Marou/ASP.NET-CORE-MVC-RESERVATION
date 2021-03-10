05b3634d7a921698e56cdf86f85d447975b7d155
Marouane Moumni <57830533+M-Marou@users.noreply.github.com>
2021-03-10 11:01:56 +0100

    Initial Setup

---
+++ Reservations/Reservations/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Reservations.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservations.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+            
+        }
+
+        public DbSet<Reservations> Reservations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=127.0.0.1;port=3306;user=root;password=root;database=YCReservationsDB")
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                .AddConsole()
+                .AddFilter(level => level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    }
+}
